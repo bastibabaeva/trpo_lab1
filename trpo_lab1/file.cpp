83bca79bbcdebcff95a915fe1aa33cf3a72b44a4
Basti <basti.babaeva@yandex.ru>
2024-09-28 17:19:07 +0700

    Добавление метода Update(), отвечающий за обновление информации о файле

--- trpo_lab1/file.cpp
+++ trpo_lab1/file.cpp
@@ -1,41 +1,41 @@
 #include "file.h"
 
-File::File(const QString& name):nameOfFile(name)//определение конструктора
+File::File(const QString& pathOfFile):path(pathOfFile)//определение конструктора
 {
-    QFileInfo fileInfo(name);//создание объекта типа QFile, в который передается его имя или значение по умолчанию
-    size=fileInfo.size();//вызов метода size() для получения информации о размере файла
-    status=fileInfo.exists();//вызов метода exists для получения информации о существовании файла
+    QFileInfo file(pathOfFile);//создание объекта типа QFile, в который передается его имя или значение по умолчанию
+    size=file.size();//вызов метода size() для получения информации о размере файла
+    status=file.exists();//вызов метода exists для получения информации о существовании файла
+}
+void File::Update() //обновление данных о файле при изменении его характеристик
+{
+    QFileInfo file(path);//создаем объект file, которому указываем текущий путь
+    status=file.exists();//устанавливаем текущий статус для этого объекта
+    size=file.size();//устанавливаем текущий размер для этого объекта
 }
 bool File::isExist()//проверка на существование файла
 {
-    QFileInfo file(nameOfFile);
+    QFileInfo file(path);
     return file.exists(); //true - если файл существует
 }
 bool File::isEmpty() //проверка на пустоту файла
 {
-    QFileInfo file(nameOfFile);
+    QFileInfo file(path);
     if(file.exists()==true && file.size()==0)
-        return true;
+        return true; //если файл существует и его размер = 0, то true
     else return false;
 }
 bool File::isChangedStatus()//проверка на изменение существования
 {
-    QFile file(nameOfFile);
+    QFile file(path);
     if(file.exists()!=status)
         return true;
     else return false;
 }
 bool File::isChangedSize() //проверка на изменение размера файла
 {
-    QFile file(nameOfFile);
+    QFile file(path);
     if(file.size()!=size)
         return true;
     else return false;
 }
-bool File::isChanged() //проверка на изменение существования или размера файла
-{
-    QFile file(nameOfFile);
-    if(file.exists()!=status || file.size()!=size)
-        return true;
-    else return false;
-}
+
--- trpo_lab1/file.h
+++ trpo_lab1/file.h
@@ -5,16 +5,16 @@
 
 class File: public QFileInfo //класс File наследуется от класса QFileInfo, чтобы получать информацию о файле
 {
-    QString nameOfFile; //имя файла
+    QString path; //путь файла
     int size;     // размер файла
     bool status; //существование файла
 public:
-    File(const QString& name);//конструктор с параметром по умолчанию
+    File(const QString& pathOfFile);//конструктор с параметром по умолчанию
+    void Update();//обновление данных о файле
 
     bool isExist(); //проверка на существование файла
     bool isEmpty();//проверка на пустоту файла
+
     bool isChangedSize(); //проверка на изменение рамера файла
     bool isChangedStatus();//проверка на изменение существование файла
-    bool isChanged();//проверка на изменение рамера и существования файла
-}
 #endif // FILE_H
